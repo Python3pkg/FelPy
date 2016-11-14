08acaae67376ec4b3d83fd7d604645d10bee5dd3
Felix <felixjohnson@hotmail.co.uk>
2016-11-14 19:58:25 +0000

    Fixed gitignore

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'FelPy',
   py_modules = ['ArrayFormat'],
-  version = '0.2',
-  description = 'An assortment of tools from Felix',
+  version = '1.0',
+  description = 'An assortment of tools from Felix - currently just an array manipulation module',
   author = 'Felix Johnson',
   author_email = 'him@felixhasa.website',
   url = 'https://github.com/12johnsonf/FelPy', # use the URL to the github repo
-  download_url = 'https://github.com/12johnsonf/FelPy/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/12johnsonf/FelPy/tarball/1.0', # I'll explain this in a second
   keywords = ['tools', 'custom', 'first'], # arbitrary keywords
   classifiers = [],
 )
