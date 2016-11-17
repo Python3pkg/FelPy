f332b2462fc6d35c1437aa3519ac0071d75b9206
Felix <felixjohnson@hotmail.co.uk>
2016-11-17 17:03:31 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'FelPy',
   py_modules = ['listFormat',"confReturn"],
-  version = '1.5',
+  version = '1.9',
   description = 'An assortment of tools from Felix - includes a list formatter, leaderboard file manager, and advanced user input reciever',
   author = 'Felix Johnson',
   author_email = 'him@felixhasa.website',
   url = 'https://github.com/12johnsonf/FelPy', # use the URL to the github repo
-  download_url = 'https://github.com/12johnsonf/FelPy/tarball/1.5', # I'll explain this in a second
+  download_url = 'https://github.com/12johnsonf/FelPy/tarball/1.9', # I'll explain this in a second
   keywords = ['tools', 'custom', 'first'], # arbitrary keywords
   classifiers = [],
 )
